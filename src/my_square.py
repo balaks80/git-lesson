ce993b96ac01c6de83c3c4e41f4d1667e5c9eb46
sb <balaks80@gmail.com>
2020-06-06 15:32:54 -0400

    Added custom input

--- src/my_square.py
+++ src/my_square.py
@@ -1,5 +1,7 @@
 
-def my_square(x):
-    return (x ** 2)
 
-print (my_square(4))
+y=int(input("Enter a number : "))
+def my_square(x):
+    return ("Square of number entered : %d") % (x ** 2)
+
+print (my_square(y))
